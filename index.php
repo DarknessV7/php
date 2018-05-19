cf70acf2b1c08cbfd26a7c8abbda3db949b29f60
DarknessV7 <diegixfezent789@gmail.com>
2018-05-19 14:24:32 -0600

    Hola mundo

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title> php </title>
+</head>
+<body>
+	<p>
+
+		<?php
+		// Declaracion de variables
+		$Pcantidad = 17;
+		$Scantidad = 6;
+		$total = $Pcantidad + $Scantidad;
+		if($total >= 18){
+			echo "Mayor a 18";
+		}else{
+			echo "Menor a 18";
+		}
+		
+
+
+
+
+
+
+
+
+		?>
+	</P>	
+
+</body>
+</html>
